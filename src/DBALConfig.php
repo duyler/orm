c5917294d8bf45d08f0d7162196f42678753377a
Mikhail Ilinsky <mail@milinsky.ru>
2025-05-16 11:21:29 +1000

    Add ask before load data fixtures command

--- src/DBALConfig.php
+++ src/DBALConfig.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace Duyler\ORM;
 
+use Cycle\Database\Config\DriverConfig;
+
 readonly class DBALConfig
 {
     public function __construct(
         public string $default = 'default',
+        /** @var class-string|null $logger */
         public ?string $logger = null,
+        /** @var array<string, string> $aliases> */
         public array $aliases = [],
+        /** @var array<string, array<string, string>> $databases */
         public array $databases = [],
+        /** @var array<string, DriverConfig> $connections */
         public array $connections = [],
     ) {}
 }
